cd9e03f2ef57c68a4c8eb4365560dfde69cd9703
michaelalemu <michael.alemu@gmail.com>
2015-07-24 11:33:33 +0200

    Changed symbol of floating button

--- bumba/include/footer.php
+++ bumba/include/footer.php
@@ -5,7 +5,7 @@
 	</footer>
 	<div class="fbtn-container">
 		<div class="fbtn-inner">
-			<a class="fbtn fbtn-red fbtn-lg" data-toggle="dropdown"><span class="fbtn-text">Add</span><span class="fbtn-ori icon">add</span><span class="fbtn-sub icon">close</span></a>
+			<a class="fbtn fbtn-red fbtn-lg" data-toggle="dropdown"><span class="fbtn-text">Add</span><span class="fbtn-ori icon">add</span><span class="fbtn-sub icon">edit</span></a>
 			<div class="fbtn-dropdown">
 				<a class="fbtn fbtn-yellow" href="#"><span class="fbtn-text">Add new post</span><span class="fa fa-pencil"></span></a>
 				<a class="fbtn fbtn-blue" href="#"><span class="fbtn-text">Add new image</span><span class="fa fa-picture-o"></span></a>
